477db08fbdc940cb266decfdd5d3154c96c0a347
maruo0501 <sagiri.maruo@gmail.com>
2020-09-22 16:37:02 +0900

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
-module ApplicationHelper
+module ApplicationHelper　# 全てのビューで利用できるようになっている
+  def full_title(page_title = "")　# メソッド定義とオプション引数
+    base_title = "Ruby on Rails Tutorial Sample App"　# 変数への代入
+    if page_title.empty?
+      base_title　# 暗黙の戻り値
+    else
+      page_title + " | " + base_title　# 文字列の結合
+    end
+  end
 end
